e2885d27e12eb2236fbbbd17d5ce5c7266a0b7d5
Manjunath Honamore <manjunath0honmore@gmail.com>
2026-03-25 09:37:28 +0530

    Fix image import case sensitivity: Test1.png

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { ArrowDown, Mail, Server, Database, Zap, Code2, Terminal } from 'lucide-react';
-import profileImg from '../assets/test1.png';
+import profileImg from '../assets/Test1.png';
 
 const GithubIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
